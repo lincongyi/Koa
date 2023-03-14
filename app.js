9cc1abb6877305a7aa88092ef77203f9aefc48a9
lincongyi <88583354@163.com>
2023-03-14 15:36:25 +0800

    remove koa-body

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Koa = require('koa')
 const cors = require('koa2-cors')
 const bodyParser = require('koa-bodyparser')
-const { koaBody } = require('koa-body')
 
 const registerRoute = require('./routes')
 
